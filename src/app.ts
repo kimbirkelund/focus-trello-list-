14ce62240337a44dc5ad433be22fbc9ef539205a
Kim Birkelund <kbi@holion.dk>
2018-11-18 14:52:10 +0100

    Reload every 5 seconds.

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,15 @@ interface ICardVm
 @autoinject
 export class App
 {
+  private _reloadTimeout;
+
   public message = "Hello World!";
   public columnClass = "pure-u-1-3";
   public isAuthenticated = this._trello.isAuthenticated;
   public cards: ICardVm[] = null;
   public columns = 3;
   public listName: string;
+  public isLoading = false;
 
   constructor(private _trello: TrelloService) { }
 
@@ -24,18 +27,33 @@ export class App
   {
     await this._trello.authenticate();
 
+    await this.reload();
+  }
+
+  public detached()
+  {
+    clearTimeout(this._reloadTimeout);
+  }
+
+  private async reload()
+  {
+    this.isLoading = true;
+
     const list = await this._trello.getList();
+
     this.listName = list.name;
-
     const cards = await this._trello.getCards();
-
     const members = await this._trello.getMembers();
-    const memberMap: { [id: string]: IMember } = {};
+
+    const memberMap: { [id: string]: IMember; } = {};
+
     members.forEach(m => memberMap[m.id] = m);
 
     const labels = await this._trello.getLabels();
     const labelMap = {};
-    labels.filter(l => l.name).forEach(l => labelMap[l.id] = l.color);
+
+    labels.filter(l => l.name)
+      .forEach(l => labelMap[l.id] = l.color);
 
     this.cards = cards.map(c =>
     {
@@ -43,8 +61,12 @@ export class App
         name: c.name,
         members: c.idMembers.map(m => memberMap[m]).map(m => m.fullName),
         color: c.idLabels && c.idLabels.length > 0 ? labelMap[c.idLabels[0]] : null
-      }
+      };
     });
+
+    this.isLoading = false;
+
+    this._reloadTimeout = setTimeout(this.reload.bind(this), 5000);
   }
 
   public addColumn()
