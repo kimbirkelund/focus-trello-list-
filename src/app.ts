263c4f4a17f8b2ed7a06307505130a19ae555125
Kim Birkelund <kbi@holion.dk>
2018-11-18 11:12:13 +0100

    Card view.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
-import { TrelloService, ICard } from "./services/trello";
+import { TrelloService, ICard, IMember } from "./services/trello";
 import { autoinject } from "aurelia-framework";
+
+interface ICardVm
+{
+  name: string;
+  members: string[];
+  color: string;
+}
 
 @autoinject
 export class App
 {
-  public message = 'Hello World!';
+  public message = "Hello World!";
   public isAuthenticated = this._trello.isAuthenticated;
-  public cards: ICard[] = null;
+  public cards: ICardVm[] = null;
+  public columns = 3;
+  public listName: string;
 
   constructor(private _trello: TrelloService) { }
 
@@ -14,7 +23,26 @@ export class App
   {
     await this._trello.authenticate();
 
-    let cards = await this._trello.getCards();
-    this.cards = cards;
+    const list = await this._trello.getList();
+    this.listName = list.name;
+
+    const cards = await this._trello.getCards();
+
+    const members = await this._trello.getMembers();
+    const memberMap: { [id: string]: IMember } = {};
+    members.forEach(m => memberMap[m.id] = m);
+
+    const labels = await this._trello.getLabels();
+    const labelMap = {};
+    labels.filter(l => l.name).forEach(l => labelMap[l.id] = l.color);
+
+    this.cards = cards.map(c =>
+    {
+      return {
+        name: c.name,
+        members: c.idMembers.map(m => memberMap[m]).map(m => m.fullName),
+        color: c.idLabels && c.idLabels.length > 0 ? labelMap[c.idLabels[0]] : null
+      }
+    });
   }
 }
